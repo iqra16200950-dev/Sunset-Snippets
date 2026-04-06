ae13043b40ef304e0513a2a69892be0b0a387b7c
Iqra Fathima <iqra16200950@gmail.com>
2026-04-06 16:24:35 +0530

    Initial commit - Sunset Snippets

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        title: "Sunset Snippets",
+        width: 214,
+        height: 228,
+        resizable: false,
+        maximizable: false,
+        fullscreenable: false,
+        frame: false,
+        transparent: true,
+        backgroundColor: "#00000000",
+        webPreferences: {
+            contextIsolation: true
+        }
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    app.quit();
+});
